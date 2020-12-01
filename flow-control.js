1c8f7ecc2d92d2ac6e4821ed4ab2bbc4c7a6e1c5
David Pires <dpires73@yahoo.com>
2020-12-01 17:14:54 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -22,10 +22,10 @@ function ageChecker(age) {
 }
 
 function ternaryTeenager(age) {
-
+/*
   return age >= 13 && age <= 19 ? "You are a teenager" : "You are not a teenager";
   }
-/*
+
   function switchAge(age) {
     switch age{
       case 13:
